fde58c8c1d8d54163d3bcdd722986abd2c71b60b
Aylton Almeida <ayltonalmeida2@gmail.com>
2020-08-13 14:01:38 -0300

    removeBuildTest

--- buildTest/static/js/main.ad5a45ef.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpfirebase-hosting"]=this["webpackJsonpfirebase-hosting"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(2),i=n.n(c);var o=function(){return r.a.createElement("div",{className:"container"},r.a.createElement("h1",null,"Hello Firebase Updated"))};n(8);i.a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(o,null)),document.getElementById("root"))}},[[3,1,2]]]);
-//# sourceMappingURL=main.ad5a45ef.chunk.js.map
